f2166c787f5c92a2ab6f14f197c4046d59310cfb
edwhereweb <edwhereweb@gmail.com>
2026-02-19 19:28:53 +0530

    feat: upgrade UploadThing v5 -> v7 (createRouteHandler, UPLOADTHING_TOKEN, generateUploadButton/Dropzone)

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,12 +1,8 @@
-import { createNextRouteHandler } from "uploadthing/next";
+import { createRouteHandler } from "uploadthing/next";
 
 import { ourFileRouter } from "./core";
 
 // Export routes for Next App Router
-export const { GET, POST } = createNextRouteHandler({
+export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
-  config: {
-    uploadthingSecret: process.env.UPLOADTHING_SECRET,
-    uploadthingId: process.env.UPLOADTHING_APP_ID,
-  },
 });
