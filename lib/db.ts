2b3bf68a64c75325bfe64e047b7d25c3234860b1
FenyRoy <fenyroy@gmail.com>
2026-02-20 12:53:37 +0530

    chore: update development script and modify PrismaClient logging configuration

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['warn', 'error'] : ['error']
-  });
+  // return new PrismaClient({
+  //   log: process.env.NODE_ENV === 'development' ? ['warn', 'error'] : ['error']
+  // });
+  return new PrismaClient();
 };
 
 declare const globalThis: {
